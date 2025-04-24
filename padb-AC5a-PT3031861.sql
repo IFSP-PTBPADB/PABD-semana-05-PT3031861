3ce233ae0d742eeb033774075c95da8b470b0452
Julia Vilela <125197027+Juliavilelas@users.noreply.github.com>
2025-04-23 22:45:39 -0300

    Create padb-AC5a-PT3031861.sql

---
+++ padb-AC5a-PT3031861.sql
@@ -0,0 +1,43 @@
+/*Questão 01. Crie uma tabela pessoa com os campos ID, nome, sobrenome e idade. 
+Crie uma cláusula de checagem para o campo idade, impedindo que valores menores que 0 sejam adicionados.
+*/
+
+CREATE TABLE pessoa (
+    ID INT,
+    nome VARCHAR(20),
+    sobrenome VARCHAR(50),
+    idade INT CHECK (idade >= 0)
+);
+
+
+/*
+Questão 02. Altere a tabela pessoa e crie uma restrição utilizando a especificação UNIQUE (ID, nome, sobrenome).
+*/
+
+ALTER TABLE pessoa
+ADD CONSTRAINT uq_id_nome_sobrenome UNIQUE (ID, nome, sobrenome);
+
+
+/*
+Questão 03. Altere a coluna idade da tabela pessoa e garanta que ela não receba valores nulos.
+*/
+
+ALTER TABLE pessoa
+ALTER COLUMN idade INT NOT NULL;
+
+
+/*
+Questão 04. Crie uma tabela endereco com os campos ID e rua.
+Adicione o campo endereco na tabela pessoa e crie uma integridade referencial a partir deste campo com a tabela endereco.
+*/
+
+CREATE TABLE endereco (
+    ID INT PRIMARY KEY,
+    rua VARCHAR(100)
+);
+
+ALTER TABLE pessoa
+ADD endereco_id INT;
+
+ALTER TABLE pessoa
+ADD CONSTRAINT fk_endereco FOREIGN KEY (endereco_id) REFERENCES endereco(ID);
